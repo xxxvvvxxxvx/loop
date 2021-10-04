d88e38211a1daea633fc7aaec379a1f3344460e7
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-10-04 06:43:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Ēriks M
+27.09
+'''
+a=["jk", "jk", "jk"]
+for jk in a:
+  print(jk)
+  
+for jk in a:
+  if jk %2==0:
+  print(jk)
+
+for jk in a:
+  if jk %2==0:
+  print(jk)
+  else:
+    print('aa')
+
+a=a
+for jk in a:
+  print('asa'jk)
+
+for jk in range(a,b,c):
+  print(jk)
